eb4d5aeabee18c818c3bdfae3c6579d45a51e5bd
erac <ilovedecone44@hotmail.fr>
2014-05-24 13:16:16 +0200

    Configured frontend added domain to config

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,54 @@ module.exports = function(grunt) {
 					'backend/**/*.js'
 				],
 				tasks: [
+					'ngconstant:'+config.server.mode,
 					'jshint:backend',
 				],
 				options: {
 					spawn: false,
 					interrupt: true,
+				},
+			},
+			frontjs: {
+				files: [
+					'frontend/js/*.js',
+					'frontend/js/**/*.js',
+					],
+				tasks: [
+					'uglify:'+config.server.mode,
+					'jshint:frontend'
+				],
+				options: {
+					spawn: false,
+					interrupt: true,
+					livereload: true,
+				},
+			},
+			less: {
+				files: [
+					'frontend/less/*.less'
+					],
+				tasks: [
+					'less:'+config.server.mode
+				],
+				options: {
+					spawn: false,
+					interrupt: true,
+					livereload: true,
+				},
+			},
+			fronthtml: {
+				files: [
+					'frontend/views/*.html',
+					'frontend/views/**/*.html'
+					],
+				tasks: [
+					'copy:views'
+				],
+				options: {
+					spawn: false,
+					interrupt: true,
+					livereload: true,
 				},
 			},
 		},
@@ -30,20 +73,125 @@ module.exports = function(grunt) {
 				'backend/*.js',
 				'backend/**/*.js'
 			],
+			frontend: [
+				'frontend/js/*.js',
+				'frontend/js/**/*.js'
+			],
+		},
+		copy: {
+			bootstrapfonts: {
+				expand: true,
+				flatten: true,
+				src: 'frontend/bower_components/bootstrap/fonts/*',
+				dest: 'frontend/public/fonts/'
+			},
+			views: {
+				expand: true,
+				flatten: true,
+				src: 'frontend/views/*',
+				dest: 'frontend/public/'
+			},
+		},
+		less: {
+			dev: {
+				options: {
+					paths: ["css"]
+				},
+				files: {
+					"frontend/public/css/default.css": "frontend/less/default.less"
+				}
+			},
+			prod: {
+				options: {
+					paths: ["css"],
+					cleancss: true,
+				},
+				files: {
+					"frontend/public/css/default.css": "frontend/less/default.less"
+				}
+			}
+		},
+		uglify: {
+			options: {
+				mangle: false,
+			},
+			dev: {
+				options: {
+					beautify: {
+						width: 80,
+						beautify: true
+					}
+				},
+				files: {
+					'frontend/public/js/index.min.js': ['frontend/js/index.js'],
+					//'frontend/public/js/login.min.js': ['frontend/login.js'],
+					'frontend/public/js/angular.min.js': [
+						'frontend/bower_components/angular/angular.js',
+						//'frontend/bower_components/angular-ui-router/release/angular-ui-router.js',
+						//'frontend/bower_components/angular-cookies/angular-cookies.js'
+					],
+				}
+			},
+			prod: {
+				files: {
+					'frontend/public/js/index.min.js': ['frontend/index.js'],
+					//'frontend/public/js/login.min.js': ['frontend/login.js'],
+					'frontend/public/js/angular.min.js': [
+						'frontend/bower_components/angular/angular.js',
+						//'frontend/bower_components/angular-ui-router/release/angular-ui-router.js',
+						//'frontend/bower_components/angular-cookies/angular-cookies.js'
+					],				
+				},
+			},
+		},
+		ngconstant: {
+			options: {
+				name: 'config',
+				dest: 'frontend/public/js/config.js',
+				constants: {
+					server: config.server,
+				},
+					values: {
+					debug: true
+				}
+			},
+			dev: {
+				constants: {
+					title: 'grunt-ng-constant-dev'
+				},
+			},
+			prod: {
+				constants: {
+					title: 'grunt-ng-constant-prod',
+					debug: false
+				}
+			},
 		},
 	});
 
 	// Load the plugin that provides the task.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-ng-constant');
+
+	grunt.registerTask('backend', [
+		'ngconstant:'+config.server.mode,
+		//'jshint:backend',
+	]);
+
+	grunt.registerTask('frontend', [
+		'jshint:frontend',
+		'less:'+config.server.mode,
+		'uglify:'+config.server.mode,
+		'copy',
+	]);
 
 		// Default task(s).
 	grunt.registerTask('default', [
-		'jshint:backend',
-		//'jshint:frontend',
-	]);
-
-	grunt.registerTask('backend', [
-		'jshint:backend',
+		'backend',
+		'frontend',
 	]);
 };
--- etc/config.js
+++ etc/config.js
@@ -1,9 +1,11 @@
 var env = require('getenv');
+var ip = require('ip');
 
 module.exports = {
 	server: {
 		name: env('NODE_SERVER_NAME','WebBuzzer'),
+		domain: env('NODE_SERVER_DOMAIN',ip.address()),
 		port: env.int('NODE_SERVER_PORT',8080),
-		mode: env('SERVER_MODE','dev')
+		mode: env('SERVER_MODE','prod')
 	}
 };
